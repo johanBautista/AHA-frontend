8d64d2742c993b6dbb0f24b6888c76336ff8e450
johan Bautista <jsbparra@gmail.com>
2019-10-24 20:58:33 +0200

    initial setup and auth

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-classes-per-file */
 import React, { Component, createContext } from 'react';
 import authService from '../services/authService';
 
@@ -7,106 +8,129 @@ const Provider = AuthContext.Provider;
 
 const AuthConsumer = AuthContext.Consumer;
 
-export const withAuth = (Comp) => {
+export const withAuth = Comp => {
   return class WithAuth extends Component {
-    
     render() {
       return (
         <AuthConsumer>
-          { 
-            ({isLoading,
-              isLoggedin,
-              user,
-              handleLogin,
-              handleLogout
-            }) => <Comp {...this.props} isLoading={isLoading} isLoggedin={isLoggedin} user={user} handleLogin={handleLogin} handleLogout={handleLogout}  />
-          }
+          {({ isLoading, isLoggedin, user, handleLogin, handleLogout, handleSignup }) => (
+            <Comp
+              {...this.props}
+              isLoading={isLoading}
+              isLoggedin={isLoggedin}
+              user={user}
+              handleLogin={handleLogin}
+              handleLogout={handleLogout}
+              handleSignup={handleSignup}
+            />
+          )}
         </AuthConsumer>
-      )
-        
+      );
     }
-  }
-}
+  };
+};
 
 export default class AuthProvider extends Component {
   state = {
     isLoggedin: false,
     user: undefined,
     isLoading: true,
-  }
+  };
 
   componentDidMount() {
-    authService.me()
-    .then((user) => {
+    authService
+      .me()
+      .then(user => {
         this.setState({
           isLoggedin: true,
           user,
           isLoading: false,
-        })
-        
+        });
+
         console.log('me', user);
       })
       .catch(() => {
         this.setState({
-          isLoading: false
-        })
-      })
+          isLoading: false,
+        });
+      });
   }
 
-  handleLogin = (user) => {
-    authService.login(user)
-      .then((loggedUser) => {
+  handleLogin = user => {
+    authService
+      .login(user)
+      .then(loggedUser => {
         this.setState({
           isLoggedin: true,
           user: loggedUser,
-          isLoading: false
-        })
+          isLoading: false,
+        });
       })
       .catch(() => {
         this.setState({
-          isLoading: false
-        })
+          isLoading: false,
+        });
+      });
+  };
+
+  handleSignup = user => {
+    authService
+      .signup(user)
+      .then(loggedUser => {
+        this.setState({
+          isLoggedin: true,
+          user: loggedUser,
+          isLoading: false,
+        });
       })
-  }
+      .catch(() => {
+        this.setState({
+          isLoading: false,
+        });
+      });
+  };
 
   handleLogout = () => {
     this.setState({
       isLoading: true,
-    })
-    authService.logout()
+    });
+    authService
+      .logout()
       .then(() => {
         this.setState({
           isLoggedin: false,
           user: undefined,
           isLoading: false,
-        })
+        });
       })
       .catch(() => {
         this.setState({
           isLoading: false,
           isLoggedin: false,
           user: undefined,
-        })
-      })
-  }
+        });
+      });
+  };
 
   render() {
-    const { isLoading, isLoggedin, user } = this.state
+    const { isLoading, isLoggedin, user } = this.state;
     const { children } = this.props;
     if (isLoading) {
-      return <div>Loading...</div>
-    } else {
-      return (
-        <Provider value={{
+      return <div>Loading...</div>;
+    }
+    return (
+      <Provider
+        value={{
           isLoading,
           isLoggedin,
           user,
           handleLogin: this.handleLogin,
           handleLogout: this.handleLogout,
-        }}>
-          {children}
-        </Provider>
-      )
-    }
+          handleSignup: this.handleSignup,
+        }}
+      >
+        {children}
+      </Provider>
+    );
   }
 }
