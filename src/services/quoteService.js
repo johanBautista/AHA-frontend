b4e73e0f678c33d2aeaf2f4c944f711d71e6c4c1
johan Bautista <jsbparra@gmail.com>
2019-11-09 16:00:08 +0100

    update

--- src/services/quoteService.js
+++ src/services/quoteService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class QuoteService {
   constructor() {
     this.axios = axios.create({
-      baseURL: 'http://localhost:3001/api',
+      baseURL: `${process.env.REACT_APP_BACKEND_BASE_URL}/api`,
       withCredentials: true,
     });
   }
