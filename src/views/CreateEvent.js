a28dd85222d7ff1b70af7a796f899e6e8402dd35
johan Bautista <jsbparra@gmail.com>
2019-11-15 01:04:58 +0100

    form event

--- src/views/CreateEvent.js
+++ src/views/CreateEvent.js
@@ -1,12 +1,18 @@
+/* eslint-disable class-methods-use-this */
 import React, { Component } from 'react';
 
 class CreateEvent extends Component {
-
   render() {
-  
     return (
       <div>
-        <h2>Aqui en crear eventos</h2>
+        <h2>Create Events</h2>
+        <form onSubmit={}>
+          <input type="text" name="title" value={} onChange={} placeholder="Title"/>
+          <input type="text" name="description" value={} onChange={} placeholder="Description"/>
+          <input type="text" name="location" value={} onChange={} placeholder="Location"/>
+          <input type="date" name="date" value={} onChange={} placeholder="Date"/>
+          <input type="submit" value="ADD NEW" className="connect-btn" />
+        </form>
       </div>
     );
   }
