a96b9500f96063b7e228793b2b5386169c4261dd
johan Bautista <jsbparra@gmail.com>
2019-11-16 08:29:42 +0100

    fix

--- src/components/search/SearchListItem.js
+++ src/components/search/SearchListItem.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const SearchListItem = ({ quote }) => {
   return (
-    <div className="style-card">
-      <li className="info-card">{quote.text}</li>
+    <div className="style-card-search">
+      <li>{quote.text}</li>
     </div>
   );
 };
